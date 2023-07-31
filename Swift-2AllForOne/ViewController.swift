36d18e35bd947240ae80c7c6c618c22136fcd349
Gabriel Toro <gatoroguzman@gmail.com>
2023-07-30 23:28:20 -0400

    Initial Commit

---
+++ Swift-2AllForOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-2AllForOne
+//
+//  Created by Gabriel Alonso Toro Guzmán on 30-07-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
